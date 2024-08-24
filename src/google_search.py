1b5b0b8d262efe684c5b0692fbeaca461bb72bdf
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-08-24 19:15:01 +0000

    update google cred

--- src/google_search.py
+++ src/google_search.py
@@ -4,11 +4,9 @@ def google_search(query, api_key, cse_id, num_results=2):
     try:
         url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cse_id}&q={query}&num={num_results}"
         response = requests.get(url)
-        response.raise_for_status()
+        response.raise_for_status()  # Vérifie si la requête a réussi
         results = response.json()
         return results.get('items', [])
-        print("resultats Google recus, traitement de la requete...")
-        
     except requests.exceptions.RequestException as e:
         print(f"Erreur lors de la recherche Google: {e}")
         return []
